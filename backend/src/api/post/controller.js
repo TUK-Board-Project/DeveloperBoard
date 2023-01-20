f60139bc170ebd5e6c1308846ae6b252f6ebf7e1
gyubeom <kwjhh704@gmail.com>
2023-01-20 17:51:24 +0900

    글 생성 함수 이름 변경

--- backend/src/api/post/controller.js
+++ backend/src/api/post/controller.js
@@ -1,8 +1,8 @@
-const{create1}=require('./query');
+const{createPosts}=require('./query');
 
 exports.save=async(ctx,next)=>{
     let body=ctx.request.body;
-    let {affectedRows,insertId}=await create1(body.user_id,body.title,body.contents);
+    let {affectedRows,insertId}=await createPosts(body.user_id,body.title,body.contents);
     ctx.body={
         id:insertId
     }
--- backend/src/api/post/query.js
+++ backend/src/api/post/query.js
@@ -1,6 +1,6 @@
 const{pool}=require('../../data/database');
 
-exports.create1=async(user_id,title,contents)=>{
+exports.createPosts=async(user_id,title,contents)=>{
     const query=`INSERT INTO post(user_id,title,contents) VALUES(?,?,?)`;
     return await pool(query,[user_id,title,contents]);
 }
