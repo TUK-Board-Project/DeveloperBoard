3d319a647af8e84ad7ad1d643deb5bbb4d09458e
gyubeom <kwjhh704@gmail.com>
2023-01-26 18:19:42 +0900

    sql문 문법 수정

--- backend/src/api/posts/query.js
+++ backend/src/api/posts/query.js
@@ -11,7 +11,7 @@ exports.getAllPosts=async(board_type)=>{
 }
 
 exports.getById=async(id)=>{
-    const query=`SELECT p.title, p.contents, p.id, p.user_id, u.email FROM posts p and user u where id=? and user.id = p.user_id`;
+    const query=`SELECT p.title, p.contents, p.id, p.user_id, u.email FROM posts p , user u where p.id=? and u.id = p.user_id`;
     return await pool(query,[id]);
 }
 
